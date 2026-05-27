d3f71d1f7a011916675f9a74aefbfce575967fe9
Alexandre Besson <alexandre.besson1@etu.univ-lorraine.fr>
2026-05-27 15:34:54 +0200

    c'est vraiment les dernière classes cette fois

--- saeobj/src/LireReseau.java
+++ saeobj/src/LireReseau.java
@@ -1,3 +1,41 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class LireReseau {
 
+    public static Graphe lire(String fichier_stations, String fichier_connexions) {
+        GrapheListe graphe = new GrapheListe();
+
+        try (BufferedReader brNodes = new BufferedReader(new FileReader(fichier_stations))) {
+            String ligne;
+            while ((ligne = brNodes.readLine()) != null) {
+                String[] infos = ligne.split(";");
+                if (infos.length >= 1) {
+                    graphe.ajouterNoeud(infos[0]);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try (BufferedReader brEdges = new BufferedReader(new FileReader(fichier_connexions))) {
+            String ligne;
+            while ((ligne = brEdges.readLine()) != null) {
+                String[] infos = ligne.split(";");
+                if (infos.length >= 3) {
+                    String depart = infos[0];
+                    String arrivee = infos[1];
+                    double distance = Double.parseDouble(infos[2]);
+
+                    graphe.ajouterArc(depart, arrivee, distance);
+                    graphe.ajouterArc(arrivee, depart, distance);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return graphe;
+    }
 }
--- saeobj/src/MainTransport.java
+++ saeobj/src/MainTransport.java
@@ -1,3 +1,39 @@
-public class MainReseau {
+import java.util.List;
 
+public class MainTransport {
+
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            System.err.println("Erreur : Deux identifiants de stations sont requis.");
+            return;
+        }
+
+        String stationDepart = args[0];
+        String stationArrivee = args[1];
+
+        String fichierNodes = "stan.nodes.txt";
+        String fichierEdges = "stan.edges.txt";
+
+        Graphe graphe = LireReseau.lire(fichierNodes, fichierEdges);
+
+        BellmanFord bf = new BellmanFord();
+
+        long startTime = System.nanoTime();
+        Valeurs resultats = bf.resoudre(graphe, stationDepart);
+        long endTime = System.nanoTime();
+
+        System.err.println("Temps execution Bellman-Ford : " + (endTime - startTime) + " ns");
+
+        List<String> chemin = resultats.calculerChemin(stationArrivee);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < chemin.size(); i++) {
+            sb.append(chemin.get(i));
+            if (i < chemin.size() - 1) {
+                sb.append(";");
+            }
+        }
+
+        System.out.println(sb.toString());
+    }
 }
