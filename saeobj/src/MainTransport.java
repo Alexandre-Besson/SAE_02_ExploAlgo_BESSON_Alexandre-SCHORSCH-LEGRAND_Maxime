7c3fb84251d90757d6e933934047f721d7fca86a
Alexandre Besson <alexandre.besson1@etu.univ-lorraine.fr>
2026-05-27 16:36:47 +0200

    correction 1

--- saeobj/src/MainTransport.java
+++ saeobj/src/MainTransport.java
@@ -1,3 +1,5 @@
+package saeobj;
+
 import java.util.List;
 
 public class MainTransport {
@@ -11,6 +13,14 @@ public class MainTransport {
         String stationDepart = args[0];
         String stationArrivee = args[1];
 
+        // Extraction du code entre crochets si Python envoie le format "Nom [CODE]"
+        if (stationDepart.contains("[") && stationDepart.contains("]")) {
+            stationDepart = stationDepart.substring(stationDepart.indexOf("[") + 1, stationDepart.indexOf("]"));
+        }
+        if (stationArrivee.contains("[") && stationArrivee.contains("]")) {
+            stationArrivee = stationArrivee.substring(stationArrivee.indexOf("[") + 1, stationArrivee.indexOf("]"));
+        }
+
         String fichierNodes = "stan.nodes.txt";
         String fichierEdges = "stan.edges.txt";
 
@@ -19,12 +29,12 @@ public class MainTransport {
         BellmanFord bf = new BellmanFord();
 
         long startTime = System.nanoTime();
-        Valeurs resultats = bf.resoudre(graphe, stationDepart);
+        Valeurs resultat = bf.resoudre(graphe, stationDepart);
         long endTime = System.nanoTime();
 
         System.err.println("Temps execution Bellman-Ford : " + (endTime - startTime) + " ns");
 
-        List<String> chemin = resultats.calculerChemin(stationArrivee);
+        List<String> chemin = resultat.calculerChemin(stationArrivee);
 
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < chemin.size(); i++) {
